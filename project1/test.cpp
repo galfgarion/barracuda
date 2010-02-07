28ed8dffc383d8a21667c40af400120784b71015
Nathanael Black <ntblack@multicore.csc.calpoly.edu>
2010-02-06 23:20:47 -0800

    Add refract to test.

--- project1/test.cpp
+++ project1/test.cpp
@@ -8,6 +8,46 @@ Vector3 reflect(Vector3 d, Vector3 n) {
    return (d - (2.0 * (d*n) * n));
 }
 
+/*
+Vector3 refract(Vector3 d, Vector3 n, double iorIn, double iorOut) {
+   d = d.normalize();
+   n = n.normalize();
+
+   double iorOutSquared = iorOut * iorOut;
+   double iorInSquared = iorIn * iorIn;
+   double dDotN = d * n;
+   double dDotNSquared = dDotN * dDotN;
+
+   double underRoot = 1 - (iorInSquared * (1 -  dDotNSquared) / iorOutSquared);
+
+   // total internal reflection
+   if(underRoot < 0)
+      return reflect(d, n);
+
+   return (1.0 / iorOut) * (iorIn * (d - (n * dDotN))) - (n * sqrt(underRoot));
+}
+*/
+
+Vector3 refract(Vector3 d, Vector3 n, double iorIn, double iorOut) {
+   d = d.normalize();
+   n = n.normalize();
+   double iorRatio = iorIn / iorOut;
+
+   // HACK i did this to get the results matching lab3,
+   // but not sure if it's correct
+   if(iorIn > iorOut) {
+      iorRatio = 1.0 / iorRatio;
+   }
+
+   double iorRatioSquared = iorRatio * iorRatio;
+
+   double underRoot = 1 - (iorRatioSquared * (1 - (d * n)*(d * n)));
+   // total internal reflection
+   if(underRoot < 0)
+      return reflect(d, n);
+
+   return iorRatio * (d - n*(d * n)) - n*sqrt(underRoot);
+}
 
 int main() {
    Vector3 ray = Vector3(-1, -1, 0);
@@ -20,4 +60,13 @@ int main() {
         << " is " << ray * normal << endl;
    cout << "Ray " << ray.c_str() << " was reflected around " << normal.c_str()
         << " to " << reflected.c_str() << endl;
+
+   Vector3 refracted = refract(ray, normal, 1, 1.33);
+
+   cout << "refracted to " << refracted.c_str() << endl;
+
+   ray = Vector3(.707, -.707, 0);
+   refracted = refract(ray, normal, 1, .9);
+   cout << "refracted to " << refracted.c_str() << endl;
+
 }
